71e2413e54aed485a7b5c8fe79f333da235ade0c
Avato <avenuefourdot@gmail.com>
2024-03-15 22:17:05 -0700

    Updated mouse layer to a more commfortable keymap

--- Personal/Keyboards/Planck/rev7/keymap.c
+++ Personal/Keyboards/Planck/rev7/keymap.c
@@ -106,9 +106,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [_FUNCZ] = LAYOUT_planck_grid(
-        _______, COLEMAKWND, COLEMAK, COLEMAK, QWERTY, _______, _______, KC_MS_BTN4, KC_MS_BTN5, KC_MS_BTN6, KC_MS_BTN7, _______,
-        _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN3, KC_DELETE,
-        _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, _______, _______, KC_MS_BTN8, KC_ACL0, KC_ACL1, KC_ACL2, _______,
+        _______, _______, _______, COLEMAKWND, COLEMAK, _______, _______, COLEMAK, QWERTY, _______, _______, _______,
+        _______, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______, _______, KC_WH_L, KC_WH_D, KC_WH_U, KC_WH_R, KC_DELETE,
+        _______, KC_BTN1, KC_BTN2, KC_BTN3, KC_BTN4, _______, _______, KC_MS_BTN5, KC_MS_BTN6, KC_MS_BTN7, KC_MS_BTN8, _______,
         _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
     ),
 
