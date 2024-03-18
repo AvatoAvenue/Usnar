aa9377de15f9490db44fd47da3225447e45d28c9
Avato <avenuefourdot@gmail.com>
2024-03-18 10:01:27 -0700

    Changed game and gamez layer

--- Personal/Keyboards/Planck/rev7/keymap.c
+++ Personal/Keyboards/Planck/rev7/keymap.c
@@ -92,17 +92,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
     ),
 
     [_GAME] = LAYOUT_planck_grid(
-        KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MUTE,
-        KC_LSFT, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_ENT,
-        KC_LCTL, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_LGUI,
-        KC_TAB, KC_Z, KC_X, KC_C, KC_SPC, KC_V, MO(_GAMEZ), KC_B, KC_N, KC_M, KC_GRV, TG(_GAME)
+        KC_ESC, KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_MUTE,
+        KC_TAB, KC_P, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TG(_GAMEZ),
+        KC_LSFT, KC_SCLN, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LGUI,
+        KC_LCTL, KC_GRAVE, KC_Z, KC_X, KC_C, KC_SPC, KC_V, MO(_GAMEZ), KC_B, KC_N, KC_M, TG(_GAME)
     ),
 
     [_GAMEZ] = LAYOUT_planck_grid(
-        _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_MPLY, _______, _______, _______, _______, KC_MUTE,
-        KC_PSCR, KC_LEFT, KC_DOWN, KC_UP, KC_RIGHT, KC_F11, KC_F12, _______, _______, _______, _______, KC_PSCR,
-        _______, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_MUTE, _______, _______, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______, MO(_GAMEZ), _______, _______, _______, KC_MPLY, _______
+        KC_F11, KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10,
+        _______, _______, _______, KC_UP, _______, _______, _______, _______, KC_COMMA, KC_DOT, KC_SLASH, TG(_GAMEZ),
+        _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, KC_MUTE, KC_MPLY, KC_PSCR, _______,
+        _______, _______, _______, _______, _______, _______, _______, MO(_GAMEZ), _______, _______, _______, KC_LALT
     ),
 
     [_FUNCZ] = LAYOUT_planck_grid(
