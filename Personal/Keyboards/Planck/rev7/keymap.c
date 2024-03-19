117f0eadc6514ea2be019caf08515ca9790f68a3
Avato <avenuefourdot@gmail.com>
2024-03-18 20:51:55 -0700

    changed game layer, again

--- Personal/Keyboards/Planck/rev7/keymap.c
+++ Personal/Keyboards/Planck/rev7/keymap.c
@@ -93,9 +93,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 
     [_GAME] = LAYOUT_planck_grid(
         KC_ESC, KC_0, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_MUTE,
-        KC_TAB, KC_P, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TG(_GAMEZ),
-        KC_LSFT, KC_SCLN, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LGUI,
-        KC_LCTL, KC_GRAVE, KC_Z, KC_X, KC_C, KC_SPC, KC_V, MO(_GAMEZ), KC_B, KC_N, KC_M, TG(_GAME)
+        KC_LSFT, KC_P, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, TG(_GAMEZ),
+        KC_LCTL, KC_SCLN, KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_LGUI,
+        KC_TAB, KC_GRAVE, KC_Z, KC_X, KC_C, KC_SPC, KC_V, MO(_GAMEZ), KC_B, KC_N, KC_M, TG(_GAME)
     ),
 
     [_GAMEZ] = LAYOUT_planck_grid(
