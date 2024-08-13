2daa050da8cadf2a65386d3934f23d8f9a826969
Avato <avenuefourdot@gmail.com>
2024-08-12 21:57:42 -0700

    changed enter

--- Personal/Keyboards/Planck/rev7/keymap.c
+++ Personal/Keyboards/Planck/rev7/keymap.c
@@ -46,6 +46,9 @@ enum planck_keycodes {
 void suspend_power_down_user(void) {
     // Apaga las luces RGB
     rgblight_disable_noeeprom();
+    #ifdef AUDIO_ENABLE
+        stop_all_notes();
+    #endif
 }
 
 //wakeup rgb
@@ -101,15 +104,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] =
 
     [_GAME] = LAYOUT_planck_grid(
         KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MUTE,
-        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_B, KC_Y, KC_U, KC_I, KC_O, KC_P, TG(_GAMEZ),
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_T, KC_M, KC_J, KC_K, KC_L, KC_SCLN, KC_LGUI,
+        KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_B, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LGUI,
+        KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_T, KC_M, KC_J, KC_K, KC_L, KC_SCLN, TG(_GAMEZ),
         KC_LCTL, KC_Z, KC_X, KC_C, KC_G, KC_SPC, KC_V, MO(_GAMEZ), KC_N, KC_H, KC_COMMA, TG(_GAME)
     ),
 
     [_GAMEZ] = LAYOUT_planck_grid(
         KC_F12, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11,
-        _______, _______, _______, KC_UP, _______, _______, _______, KC_COMMA, KC_DOT, KC_SLASH, _______, TG(_GAMEZ),
-        _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, KC_UP, _______, _______, _______, KC_COMMA, KC_DOT, KC_SLASH, _______, _______,
+        _______, _______, KC_LEFT, KC_DOWN, KC_RIGHT, _______, _______, _______, _______, _______, _______, TG(_GAMEZ),
         _______, _______, _______, _______, _______, _______, _______, MO(_GAMEZ), KC_MUTE, KC_MPLY, KC_PSCR, KC_LALT
     ),
 
