9a4cdb41054d2fc83fe72e9c498eda7bf00ca8ab
avatoavenue <avenuefourdot@gmail.com>
2024-12-16 22:18:09 -0800

    modified:   TextEditors/nvim/lua/avato/remap.lua

--- TextEditors/nvim/lua/avato/remap.lua
+++ TextEditors/nvim/lua/avato/remap.lua
@@ -5,7 +5,7 @@
 vim.g.mapleader = " "
 
 -- shortcut to Ex
-vim.keymap.set("n", "<leader>ge", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>te", vim.cmd.Ex)
 
 -- clear highlights after esc in normal mode
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
